6408744949348804cf3d7ac953ab5238c43413e2
mennanoseer <mennanoseer@ejust.edu.eg>
2023-03-16 14:37:52 -0700

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function to know how to use printf
+ *
+ * Return: 0 when true
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
