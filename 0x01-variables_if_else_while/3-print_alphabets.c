2be14c5f24d14fbedb23c5cf58425a857339eefd
mennanoseer <mennanoseer@ejust.edu.eg>
2023-03-17 10:16:28 -0700

    t3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - lowercase and uppercase alphabet
+ *
+ * Return: 0 when true
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
