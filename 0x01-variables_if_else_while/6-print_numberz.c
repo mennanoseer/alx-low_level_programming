e9572f16acb1d711ff9a19e91fb941aa117e9ae4
mennanoseer <mennanoseer@ejust.edu.eg>
2023-03-17 12:41:43 -0700

    t6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - single digits
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
